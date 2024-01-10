c191d7780643eda50d98ae2f7637b338baec1b64
Marcos Flavio <mfghost69@gmail.com>
2024-01-10 20:24:30 -0300

    Feat :add java and cmake utils

--- config.el
+++ config.el
@@ -93,6 +93,9 @@
 
 (require 'dap-gdb-lldb)
 
+(require 'lsp-java)
+(add-hook 'java-mode-hook #'lsp)
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
